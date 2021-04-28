ee2a873f19444574436fa0007a7a95a4825c647e
Raphael Mantilha <raphael.mantilha@gmail.com>
2021-04-28 20:19:57 -0300

    ajustando o arquivo env.rb

--- web/features/support/env.rb
+++ web/features/support/env.rb
@@ -18,18 +18,19 @@ when "chrome_headless"
   # O código abaixo foi retirado do código-fonted o Capybara no Github e foi ajustado para funcionar com o "ruby web agent"
   # que vamos usar para rodar os testes no Jenkins
   Capybara.register_driver :selenium_chrome_headless do |app|
-    #Capybara.register_driver :selenium_chrome_headless do |app|
-    Capybara::Selenium::Driver.load_selenium
+    version = Capybara::Selenium::Driver.load_selenium
+    options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
     browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.args << "--headless"
-      opts.args << "--disable-gpu"
-      opts.args << "--no-sandbox" # eu incluí esta linha. É importante para rodar em headless (pincipalmente dentr de container)
-      opts.args << "--desable-dev-shm-usage" # eu incluí esta linha. É fundamental para o Chrome usar os recursos
-      # de cache em disco e não em memória. Memória é escassa dentro da VM quando usamos Docker Toolbox.
-      opts.args << "--disable-site-isolation-trials"
+      opts.add_argument("--headless")
+      opts.add_argument("--disable-gpu")
+      opts.add_argument("--disable-site-isolation-trials")
+      opts.add_argument("--no-sandbox")
+      opts.add_argument("--disable-dev-shm-usage")
     end
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+
+    Capybara::Selenium::Driver.new(app, **Hash[:browser => :chrome, options_key => browser_options])
   end
+
   #   version = Capybara::Selenium::Driver.load_selenium
   #   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   #   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
